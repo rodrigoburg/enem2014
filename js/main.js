acb7fe6ea51bb9ebcb1d59bcecf384fd3a33f95e
Rodrigo Burgarelli <rodrigo.burgarelli1@gmail.com>
2015-08-05 21:40:50 -0300

    cor nova no input

--- js/main.js
+++ js/main.js
@@ -102,7 +102,7 @@ function coloca_footer(callback) {
     $('#tabela_real tfoot th').each( function () {
         var title = $('#tabela_real thead th').eq( $(this).index() ).text();
         if (title == "Nome da escola" || title == "Município") {
-            $(this).html( '<input type="text" style="width:100%" placeholder="Buscar" />' );
+            $(this).html( '<input type="text" style="color:dimgray" placeholder="Buscar" />' );
         } else if (title == "UF") {
             var select = '<select name="estado"><option value=""><b>Todos</b></option><option value="ac">Acre</option><option value="al">Alagoas</option><option value="am">Amazonas</option><option value="ap">Amapá</option><option value="ba">Bahia</option><option value="ce">Ceará</option><option value="df">Distrito Federal</option><option value="es">Espírito Santo</option><option value="go">Goiás</option><option value="ma">Maranhão</option><option value="mt">Mato Grosso</option><option value="ms">Mato Grosso do Sul</option><option value="mg">Minas Gerais</option><option value="pa">Pará</option><option value="pb">Paraíba</option><option value="pr">Paraná</option><option value="pe">Pernambuco</option><option value="pi">Piauí</option><option value="rj">Rio de Janeiro</option><option value="rn">Rio Grande do Norte</option><option value="ro">Rondônia</option><option value="rs">Rio Grande do Sul</option><option value="rr">Roraima</option><option value="sc">Santa Catarina</option><option value="se">Sergipe</option><option value="sp">São Paulo</option><option value="to">Tocantins</option></select>'
             $(this).html(select);
